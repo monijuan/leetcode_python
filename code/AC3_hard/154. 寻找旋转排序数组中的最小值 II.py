98937f58cb59be37b607166ac5feec4bc5c570b7
monijuan <1019646292@qq.com>
2021-08-29 22:41:02 +0800

    153 154 offer 11

---
+++ code/AC3_hard/154. 寻找旋转排序数组中的最小值 II.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/8/29 21:44
+# @Author  : 模拟卷
+# @Github  : https://github.com/monijuan
+# @CSDN    : https://blog.csdn.net/qq_34451909
+# @File    : 154. 寻找旋转排序数组中的最小值 II.py
+# @Software: PyCharm 
+# ===================================
+"""已知一个长度为 n 的数组，预先按照升序排列，经由 1 到 n 次 旋转 后，得到输入数组。例如，原数组 nums = [0,1,4,4,5,6,7] 在变化后可能得到：
+若旋转 4 次，则可以得到 [4,5,6,7,0,1,4]
+若旋转 7 次，则可以得到 [0,1,4,4,5,6,7]
+注意，数组 [a[0], a[1], a[2], ..., a[n-1]] 旋转一次 的结果为数组 [a[n-1], a[0], a[1], a[2], ..., a[n-2]] 。
+
+给你一个可能存在 重复 元素值的数组 nums ，它原来是一个升序排列的数组，并按上述情形进行了多次旋转。请你找出并返回数组中的 最小元素 。
+
+ 
+
+示例 1：
+
+输入：nums = [1,3,5]
+输出：1
+示例 2：
+
+输入：nums = [2,2,2,0,1]
+输出：0
+ 
+
+提示：
+
+n == nums.length
+1 <= n <= 5000
+-5000 <= nums[i] <= 5000
+nums 原来是一个升序排序的数组，并进行了 1 至 n 次旋转
+ 
+
+进阶：
+
+这道题是 寻找旋转排序数组中的最小值 的延伸题目。
+允许重复会影响算法的时间复杂度吗？会如何影响，为什么？
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+import time
+from typing import List
+
+
+class Solution:
+    def __init__(self):
+        pass
+
+    def findMin(self, nums: List[int]) -> int:
+        return min(nums)
+
+
+def test(data_test):
+    s = Solution()
+    return s.findMin(*data_test)
+
+
+if __name__ == '__main__':
+    datas = [
+        [],
+    ]
+    for data_test in datas:
+        t0 = time.time()
+        print('-' * 50)
+        print('input:', data_test)
+        print('output:', test(data_test))
+        print(f'use time:{time.time() - t0}s')
---
+++ code/Offer/Offer_day05_11. 旋转数组的最小数字.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/8/29 21:41
+# @Author  : 模拟卷
+# @Github  : https://github.com/monijuan
+# @CSDN    : https://blog.csdn.net/qq_34451909
+# @File    : Offer_day05_11. 旋转数组的最小数字.py
+# @Software: PyCharm 
+# ===================================
+"""把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素。例如，数组 [3,4,5,1,2] 为 [1,2,3,4,5] 的一个旋转，该数组的最小值为1。  
+
+示例 1：
+
+输入：[3,4,5,1,2]
+输出：1
+示例 2：
+
+输入：[2,2,2,0,1]
+输出：0
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+import time
+from typing import List
+# 本题与主站 154 题相同
+
+class Solution:
+    def __init__(self):
+        pass
+
+    def minArray(self, numbers: List[int]) -> int:
+        return min(numbers)
+
+
+def test(data_test):
+    s = Solution()
+    return s.minArray(*data_test)
+
+
+if __name__ == '__main__':
+    datas = [
+        [],
+    ]
+    for data_test in datas:
+        t0 = time.time()
+        print('-' * 50)
+        print('input:', data_test)
+        print('output:', test(data_test))
+        print(f'use time:{time.time() - t0}s')
