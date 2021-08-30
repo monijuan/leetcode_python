5d9015a8b15aed0844181606e57e182f95af1f85
monijuan <1019646292@qq.com>
2021-08-30 22:55:13 +0800

    add offer 63

---
+++ code/Offer/Offer_day07_26. 树的子结构.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/8/30 22:34
+# @Author  : 模拟卷
+# @Github  : https://github.com/monijuan
+# @CSDN    : https://blog.csdn.net/qq_34451909
+# @File    : Offer_day07_26. 树的子结构.py
+# @Software: PyCharm 
+# ===================================
+"""
+"""
+import time
+from typing import List
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def __init__(self):
+        pass
+
+    def isSame(self, A: TreeNode, B: TreeNode) -> bool:
+        pass
+
+    def isSubStructure(self, A: TreeNode, B: TreeNode) -> bool:
+        pass
+
+
+def test(data_test):
+    s = Solution()
+    return s.isSubStructure(*data_test)
+
+
+def test_obj(data_test):
+    result = [None]
+    obj = Solution(*data_test[1][0])
+    for fun, data in zip(data_test[0][1::], data_test[1][1::]):
+        if data:
+            res = obj.__getattribute__(fun)(*data)
+        else:
+            res = obj.__getattribute__(fun)()
+        result.append(res)
+    return result
+
+
+if __name__ == '__main__':
+    datas = [
+        [],
+    ]
+    for data_test in datas:
+        t0 = time.time()
+        print('-' * 50)
+        print('input:', data_test)
+        print('output:', test(data_test))
+        print(f'use time:{time.time() - t0}s')
