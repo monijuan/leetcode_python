cb1a4a955c30079c366ff73afb6326414c90bbdf
monijuan <yinzhijie@fanhaninfo.com>
2021-09-03 09:07:34 +0800

    ac intvw 17.14

---
+++ code/Interview/面试题 17.14. 最小K个数.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/9/3 9:04
+# @Github  : https://github.com/monijuan
+# @CSDN    : https://blog.csdn.net/qq_34451909
+# @File    : 面试题 17.14. 最小K个数.py
+# @Software: PyCharm
+# ===================================
+"""设计一个算法，找出数组中最小的k个数。以任意顺序返回这k个数均可。
+
+示例：
+
+输入： arr = [1,3,5,7,2,4,6,8], k = 4
+输出： [1,2,3,4]
+提示：
+
+0 <= len(arr) <= 100000
+0 <= k <= min(100000, len(arr))
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/smallest-k-lcci
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+import time
+from typing import List
+
+
+class Solution:
+    def __init__(self):
+        pass
+
+    def smallestK(self, arr: List[int], k: int) -> List[int]:
+        return sorted(arr)[:k]
+
+
+def test(data_test):
+    s = Solution()
+    return s.smallestK(*data_test)
+
+
+def test_obj(data_test):
+    result = [None]
+    obj = Solution(*data_test[1][0])
+    for fun, data in zip(data_test[0][1::], data_test[1][1::]):
+        if data:
+            res = obj.__getattribute__(fun)(*data)
+        else:
+            res = obj.__getattribute__(fun)()
+        result.append(res)
+    return result
+
+
+if __name__ == '__main__':
+    datas = [
+        [[1,2,3],0],
+        [[1,3,5,7,2,4,6,8],4],
+    ]
+    for data_test in datas:
+        t0 = time.time()
+        print('-' * 50)
+        print('input:', data_test)
+        print('output:', test(data_test))
+        print(f'use time:{time.time() - t0}s')
