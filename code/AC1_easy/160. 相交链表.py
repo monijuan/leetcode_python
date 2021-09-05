c5708f40296fb35c76824d5bc6808b815a16a31b
monijuan <1019646292@qq.com>
2021-09-05 09:51:51 +0800

    160  o52

---
+++ code/AC1_easy/160. 相交链表.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/9/5 9:39
+# @Author  : 模拟卷
+# @Github  : https://github.com/monijuan
+# @CSDN    : https://blog.csdn.net/qq_34451909
+# @File    : 160. 相交链表.py
+# @Software: PyCharm 
+# ===================================
+"""
+"""
+import time
+from typing import List
+
+
+class Solution:
+    def __init__(self):
+        pass
+
+    def getResult(self, args):
+        return
+
+
+def test(data_test):
+    s = Solution()
+    return s.getResult(*data_test)
+
+
+def test_obj(data_test):
+    result = [None]
+    obj = Solution(*data_test[1][0])
+    for fun, data in zip(data_test[0][1::], data_test[1][1::]):
+        if data:
+            res = obj.__getattribute__(fun)(*data)
+        else:
+            res = obj.__getattribute__(fun)()
+        result.append(res)
+    return result
+
+
+if __name__ == '__main__':
+    datas = [
+        [],
+    ]
+    for data_test in datas:
+        t0 = time.time()
+        print('-' * 50)
+        print('input:', data_test)
+        print('output:', test(data_test))
+        print(f'use time:{time.time() - t0}s')
