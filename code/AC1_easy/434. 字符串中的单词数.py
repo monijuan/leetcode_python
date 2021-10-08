9b02a6a09cc69e31ca145cfa07d260aa342ebd7c
monijuan <yinzhijie@fanhaninfo.com>
2021-10-08 17:07:51 +0800

    434 187

---
+++ code/AC1_easy/434. 字符串中的单词数.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/10/8 17:02
+# @Github  : https://github.com/monijuan
+# @CSDN    : https://blog.csdn.net/qq_34451909
+# @File    : 434. 字符串中的单词数.py
+# @Software: PyCharm
+# ===================================
+"""统计字符串中的单词个数，这里的单词指的是连续的不是空格的字符。
+
+请注意，你可以假定字符串里不包括任何不可打印的字符。
+
+示例:
+
+输入: "Hello, my name is John"
+输出: 5
+解释: 这里的单词是指连续的不是空格的字符，所以 "Hello," 算作 1 个单词。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/number-of-segments-in-a-string
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+from leetcode_python.utils import *
+
+
+class Solution:
+    def __init__(self):
+        pass
+
+    def countSegments(self, s: str) -> int:
+        print(s.split(' '))
+        return len([x for x in s.split(' ') if x != ''])
+
+
+def test(data_test):
+    s = Solution()
+    return s.countSegments(*data_test)
+
+
+def test_obj(data_test):
+    result = [None]
+    obj = Solution(*data_test[1][0])
+    for fun, data in zip(data_test[0][1::], data_test[1][1::]):
+        if data:
+            res = obj.__getattribute__(fun)(*data)
+        else:
+            res = obj.__getattribute__(fun)()
+        result.append(res)
+    return result
+
+
+if __name__ == '__main__':
+    datas = [
+        [""],
+    ]
+    for data_test in datas:
+        t0 = time.time()
+        print('-' * 50)
+        print('input:', data_test)
+        print('output:', test(data_test))
+        print(f'use time:{time.time() - t0}s')
