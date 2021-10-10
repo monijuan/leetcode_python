04dedb6906b7eae85954b1f370b50ef4fa453124
monijuan <1019646292@qq.com>
2021-10-10 22:09:02 +0800

    1436 441

---
+++ code/AC1_easy/441. 排列硬币.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/10/10 22:06
+# @Author  : 模拟卷
+# @Github  : https://github.com/monijuan
+# @CSDN    : https://blog.csdn.net/qq_34451909
+# @File    : 441. 排列硬币.py
+# @Software: PyCharm 
+# ===================================
+"""你总共有 n 枚硬币，并计划将它们按阶梯状排列。对于一个由 k 行组成的阶梯，其第 i 行必须正好有 i 枚硬币。阶梯的最后一行 可能 是不完整的。
+
+给你一个数字 n ，计算并返回可形成 完整阶梯行 的总行数。
+
+ 
+
+示例 1：
+
+
+输入：n = 5
+输出：2
+解释：因为第三行不完整，所以返回 2 。
+示例 2：
+
+
+输入：n = 8
+输出：3
+解释：因为第四行不完整，所以返回 3 。
+ 
+
+提示：
+
+1 <= n <= 231 - 1
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/arranging-coins
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+from leetcode_python.utils import *
+
+
+class Solution:
+    def __init__(self):
+        pass
+
+    def arrangeCoins(self, n: int) -> int:
+        return int(((1 + 8 * n)**0.5 - 1) / 2)
+
+
+def test(data_test):
+    s = Solution()
+    return s.getResult(*data_test)
+
+
+def test_obj(data_test):
+    result = [None]
+    obj = Solution(*data_test[1][0])
+    for fun, data in zip(data_test[0][1::], data_test[1][1::]):
+        if data:
+            res = obj.__getattribute__(fun)(*data)
+        else:
+            res = obj.__getattribute__(fun)()
+        result.append(res)
+    return result
+
+
+if __name__ == '__main__':
+    datas = [
+        [],
+    ]
+    for data_test in datas:
+        t0 = time.time()
+        print('-' * 50)
+        print('input:', data_test)
+        print('output:', test(data_test))
+        print(f'use time:{time.time() - t0}s')
