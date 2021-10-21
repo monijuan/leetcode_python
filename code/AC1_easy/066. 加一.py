dd764ec42bcbf65cab3c3e2a8183b6abef0a9501
monijuan <yinzhijie@fanhaninfo.com>
2021-10-21 09:21:15 +0800

    120 066

---
+++ code/AC1_easy/066. 加一.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/10/21 9:00
+# @Github  : https://github.com/monijuan
+# @CSDN    : https://blog.csdn.net/qq_34451909
+# @File    : 066. 加一.py
+# @Software: PyCharm
+# ===================================
+"""给定一个由 整数 组成的 非空 数组所表示的非负整数，在该数的基础上加一。
+
+最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+
+你可以假设除了整数 0 之外，这个整数不会以零开头。
+
+ 
+
+示例 1：
+
+输入：digits = [1,2,3]
+输出：[1,2,4]
+解释：输入数组表示数字 123。
+示例 2：
+
+输入：digits = [4,3,2,1]
+输出：[4,3,2,2]
+解释：输入数组表示数字 4321。
+示例 3：
+
+输入：digits = [0]
+输出：[1]
+ 
+
+提示：
+
+1 <= digits.length <= 100
+0 <= digits[i] <= 9
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/plus-one
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+from leetcode_python.utils import *
+
+
+class Solution:
+    def __init__(self):
+        pass
+
+    def plusOne(self, digits: List[int]) -> List[int]:
+        """拼接字符串，转数字后加一，再转回来分割"""
+        return [int(x) for x in str((int(''.join([str(i) for i in digits])))+1)]
+
+
+def test(data_test):
+    s = Solution()
+    return s.plusOne(*data_test)
+
+
+def test_obj(data_test):
+    result = [None]
+    obj = Solution(*data_test[1][0])
+    for fun, data in zip(data_test[0][1::], data_test[1][1::]):
+        if data:
+            res = obj.__getattribute__(fun)(*data)
+        else:
+            res = obj.__getattribute__(fun)()
+        result.append(res)
+    return result
+
+
+if __name__ == '__main__':
+    datas = [
+        [[4,3,2,1]],
+    ]
+    for data_test in datas:
+        t0 = time.time()
+        print('-' * 50)
+        print('input:', data_test)
+        print('output:', test(data_test))
+        print(f'use time:{time.time() - t0}s')
