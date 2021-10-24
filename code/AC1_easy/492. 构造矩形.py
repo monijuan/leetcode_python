63638c9d186dd39e4af83786e69b969ba1be334f
monijuan <1019646292@qq.com>
2021-10-24 23:19:17 +0800

    073 638 492

---
+++ code/AC1_easy/492. 构造矩形.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/10/23 21:37
+# @Author  : 模拟卷
+# @Github  : https://github.com/monijuan
+# @CSDN    : https://blog.csdn.net/qq_34451909
+# @File    : 492. 构造矩形.py
+# @Software: PyCharm 
+# ===================================
+"""作为一位web开发者， 懂得怎样去规划一个页面的尺寸是很重要的。 现给定一个具体的矩形页面面积，你的任务是设计一个长度为 L 和宽度为 W 且满足以下要求的矩形的页面。要求：
+
+1. 你设计的矩形页面必须等于给定的目标面积。
+
+2. 宽度 W 不应大于长度 L，换言之，要求 L >= W 。
+
+3. 长度 L 和宽度 W 之间的差距应当尽可能小。
+你需要按顺序输出你设计的页面的长度 L 和宽度 W。
+
+示例：
+
+输入: 4
+输出: [2, 2]
+解释: 目标面积是 4， 所有可能的构造方案有 [1,4], [2,2], [4,1]。
+但是根据要求2，[1,4] 不符合要求; 根据要求3，[2,2] 比 [4,1] 更能符合要求. 所以输出长度 L 为 2， 宽度 W 为 2。
+说明:
+
+给定的面积不大于 10,000,000 且为正整数。
+你设计的页面的长度和宽度必须都是正整数。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/construct-the-rectangle
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+from leetcode_python.utils import *
+
+
+class Solution:
+    def __init__(self):
+        pass
+
+    def constructRectangle(self, area: int) -> List[int]:
+        sqrtnum = int(area**0.5)
+        while area % sqrtnum:
+            sqrtnum-=1
+        return [area//sqrtnum,sqrtnum]
+
+
+def test(data_test):
+    s = Solution()
+    return s.getResult(*data_test)
+
+
+def test_obj(data_test):
+    result = [None]
+    obj = Solution(*data_test[1][0])
+    for fun, data in zip(data_test[0][1::], data_test[1][1::]):
+        if data:
+            res = obj.__getattribute__(fun)(*data)
+        else:
+            res = obj.__getattribute__(fun)()
+        result.append(res)
+    return result
+
+
+if __name__ == '__main__':
+    datas = [
+        [],
+    ]
+    for data_test in datas:
+        t0 = time.time()
+        print('-' * 50)
+        print('input:', data_test)
+        print('output:', test(data_test))
+        print(f'use time:{time.time() - t0}s')
