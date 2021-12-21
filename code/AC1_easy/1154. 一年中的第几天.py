edf778ccc57cc559ddd329d151a6775618893dd8
monijuan <yinzhijie@fanhaninfo.com>
2021-12-21 17:30:50 +0800

    1154 475

---
+++ code/AC1_easy/1154. 一年中的第几天.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/12/21 8:55
+# @Github  : https://github.com/monijuan
+# @CSDN    : https://blog.csdn.net/qq_34451909
+# @File    : 1154. 一年中的第几天.py
+# @Software: PyCharm
+# ===================================
+"""给你一个字符串 date ，按 YYYY-MM-DD 格式表示一个 现行公元纪年法 日期。请你计算并返回该日期是当年的第几天。
+
+通常情况下，我们认为 1 月 1 日是每年的第 1 天，1 月 2 日是每年的第 2 天，依此类推。每个月的天数与现行公元纪年法（格里高利历）一致。
+
+ 
+
+示例 1：
+
+输入：date = "2019-01-09"
+输出：9
+示例 2：
+
+输入：date = "2019-02-10"
+输出：41
+示例 3：
+
+输入：date = "2003-03-01"
+输出：60
+示例 4：
+
+输入：date = "2004-03-01"
+输出：61
+ 
+
+提示：
+
+date.length == 10
+date[4] == date[7] == '-'，其他的 date[i] 都是数字
+date 表示的范围从 1900 年 1 月 1 日至 2019 年 12 月 31 日
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/day-of-the-year
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+from leetcode_python.utils import *
+
+
+class Solution:
+    def __init__(self):
+        pass
+
+    def dayOfYear(self, date: str) -> int:
+        year, month, day = [int(x) for x in date.split("-")]
+
+        amount = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
+            amount[1] += 1
+
+        ans = sum(amount[:month - 1])
+        return ans + day
+
+
+def test(data_test):
+    s = Solution()
+    data = data_test  # normal
+    # data = [list2node(data_test[0])]  # list转node
+    return s.dayOfYear(*data)
+
+
+def test_obj(data_test):
+    result = [None]
+    obj = Solution(*data_test[1][0])
+    for fun, data in zip(data_test[0][1::], data_test[1][1::]):
+        if data:
+            res = obj.__getattribute__(fun)(*data)
+        else:
+            res = obj.__getattribute__(fun)()
+        result.append(res)
+    return result
+
+
+if __name__ == '__main__':
+    datas = [
+        [],
+    ]
+    for data_test in datas:
+        t0 = time.time()
+        print('-' * 50)
+        print('input:', data_test)
+        print('output:', test(data_test))
+        print(f'use time:{time.time() - t0}s')
