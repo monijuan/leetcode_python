9b0256c450c15651f5a855d3cf7ea5323224355b
monijuan <yinzhijie@fanhaninfo.com>
2022-02-10 13:42:53 +0800

    2006 1405 1447 1001

---
+++ code/AC2_normal/1405. 最长快乐字符串.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/2/7 8:57
+# @Github  : https://github.com/monijuan
+# @CSDN    : https://blog.csdn.net/qq_34451909
+# @File    : 1405. 最长快乐字符串.py
+# @Software: PyCharm
+# ===================================
+"""如果字符串中不含有任何 'aaa'，'bbb' 或 'ccc' 这样的字符串作为子串，那么该字符串就是一个「快乐字符串」。
+
+给你三个整数 a，b ，c，请你返回 任意一个 满足下列全部条件的字符串 s：
+
+s 是一个尽可能长的快乐字符串。
+s 中 最多 有a 个字母 'a'、b 个字母 'b'、c 个字母 'c' 。
+s 中只含有 'a'、'b' 、'c' 三种字母。
+如果不存在这样的字符串 s ，请返回一个空字符串 ""。
+
+ 
+
+示例 1：
+
+输入：a = 1, b = 1, c = 7
+输出："ccaccbcc"
+解释："ccbccacc" 也是一种正确答案。
+示例 2：
+
+输入：a = 2, b = 2, c = 1
+输出："aabbc"
+示例 3：
+
+输入：a = 7, b = 1, c = 0
+输出："aabaa"
+解释：这是该测试用例的唯一正确答案。
+ 
+
+提示：
+
+0 <= a, b, c <= 100
+a + b + c > 0
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/longest-happy-string
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+from leetcode_python.utils import *
+
+
+class Solution:
+    def longestDiverseString(self, a: int, b: int, c: int) -> str:
+        cnt = {'a':a,'b':b,'c':c}
+        res = ''
+        while True:
+            _,mid,most = sorted(cnt.keys(),key=lambda x:cnt[x])
+            if (len(res)<2 or not res[-2]==res[-1]==most) and cnt[most]:
+                res+=most
+                cnt[most]-=1
+            elif cnt[mid]:
+                res+=mid
+                cnt[mid]-=1
+            else:
+                break
+        return res
+
+def test(data_test):
+    s = Solution()
+    data = data_test  # normal
+    # data = [list2node(data_test[0])]  # list转node
+    return s.longestDiverseString(*data)
+
+
+def test_obj(data_test):
+    result = [None]
+    obj = Solution(*data_test[1][0])
+    for fun, data in zip(data_test[0][1::], data_test[1][1::]):
+        if data:
+            res = obj.__getattribute__(fun)(*data)
+        else:
+            res = obj.__getattribute__(fun)()
+        result.append(res)
+    return result
+
+
+if __name__ == '__main__':
+    datas = [
+        [],
+    ]
+    for data_test in datas:
+        t0 = time.time()
+        print('-' * 50)
+        print('input:', data_test)
+        print('output:', test(data_test))
+        print(f'use time:{time.time() - t0}s')
