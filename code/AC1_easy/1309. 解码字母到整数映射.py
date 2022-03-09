21f0a43c010a2f77e27f10d93446483a29ade17b
monijuan <yinzhijie@fanhaninfo.com>
2022-03-09 10:32:57 +0800

    1309 953'

---
+++ code/AC1_easy/1309. 解码字母到整数映射.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/3/9 10:06
+# @Github  : https://github.com/monijuan
+# @CSDN    : https://blog.csdn.net/qq_34451909
+# @File    : 1309. 解码字母到整数映射.py
+# @Software: PyCharm
+# ===================================
+"""给你一个字符串 s，它由数字（'0' - '9'）和 '#' 组成。我们希望按下述规则将 s 映射为一些小写英文字符：
+
+字符（'a' - 'i'）分别用（'1' - '9'）表示。
+字符（'j' - 'z'）分别用（'10#' - '26#'）表示。 
+返回映射之后形成的新字符串。
+
+题目数据保证映射始终唯一。
+
+ 
+
+示例 1：
+
+输入：s = "10#11#12"
+输出："jkab"
+解释："j" -> "10#" , "k" -> "11#" , "a" -> "1" , "b" -> "2".
+示例 2：
+
+输入：s = "1326#"
+输出："acz"
+示例 3：
+
+输入：s = "25#"
+输出："y"
+示例 4：
+
+输入：s = "12345678910#11#12#13#14#15#16#17#18#19#20#21#22#23#24#25#26#"
+输出："abcdefghijklmnopqrstuvwxyz"
+ 
+
+提示：
+
+1 <= s.length <= 1000
+s[i] 只包含数字（'0'-'9'）和 '#' 字符。
+s 是映射始终存在的有效字符串。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/decrypt-string-from-alphabet-to-integer-mapping
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+from leetcode_python.utils import *
+
+
+def get(st):
+    return chr(int(st) + 96)
+class Solution:
+    def freqAlphabets(self, s: str) -> str:
+        i, res = 0, ""
+        while i < len(s):
+            if i + 2 < len(s) and s[i + 2] == '#':
+                res += get(s[i : i + 2])
+                i += 2
+            else:
+                res += get(s[i])
+            i += 1
+        return res
+
+def test(data_test):
+    s = Solution()
+    data = data_test  # normal
+    # data = [list2node(data_test[0])]  # list转node
+    return s.getResult(*data)
+
+
+def test_obj(data_test):
+    result = [None]
+    obj = Solution(*data_test[1][0])
+    for fun, data in zip(data_test[0][1::], data_test[1][1::]):
+        if data:
+            res = obj.__getattribute__(fun)(*data)
+        else:
+            res = obj.__getattribute__(fun)()
+        result.append(res)
+    return result
+
+
+if __name__ == '__main__':
+    datas = [
+        [],
+    ]
+    for data_test in datas:
+        t0 = time.time()
+        print('-' * 50)
+        print('input:', data_test)
+        print('output:', test(data_test))
+        print(f'use time:{time.time() - t0}s')
