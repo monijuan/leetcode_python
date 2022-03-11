aa7de5a0818727e9d74267d484986b1298df0af9
monijuan <yinzhijie@fanhaninfo.com>
2022-03-11 10:02:57 +0800

    303

---
+++ code/AC1_easy/303. 区域和检索 - 数组不可变.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/3/11 9:54
+# @Github  : https://github.com/monijuan
+# @CSDN    : https://blog.csdn.net/qq_34451909
+# @File    : 303. 区域和检索 - 数组不可变.py
+# @Software: PyCharm
+# ===================================
+"""给定一个整数数组  nums，处理以下类型的多个查询:
+
+计算索引 left 和 right （包含 left 和 right）之间的 nums 元素的 和 ，其中 left <= right
+实现 NumArray 类：
+
+NumArray(int[] nums) 使用数组 nums 初始化对象
+int sumRange(int i, int j) 返回数组 nums 中索引 left 和 right 之间的元素的 总和 ，包含 left 和 right 两点（也就是 nums[left] + nums[left + 1] + ... + nums[right] )
+ 
+
+示例 1：
+
+输入：
+["NumArray", "sumRange", "sumRange", "sumRange"]
+[[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+输出：
+[null, 1, -1, -3]
+
+解释：
+NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+numArray.sumRange(0, 2); // return 1 ((-2) + 0 + 3)
+numArray.sumRange(2, 5); // return -1 (3 + (-5) + 2 + (-1))
+numArray.sumRange(0, 5); // return -3 ((-2) + 0 + 3 + (-5) + 2 + (-1))
+ 
+
+提示：
+
+1 <= nums.length <= 104
+-105 <= nums[i] <= 105
+0 <= i <= j < nums.length
+最多调用 104 次 sumRange 方法
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/range-sum-query-immutable
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+from leetcode_python.utils import *
+
+
+class NumArray:
+    def __init__(self, nums: List[int]):
+        self.sums = [nums[0]]
+        for num in nums[1:]:
+            self.sums.append(self.sums[-1]+num)
+
+    def sumRange(self, left: int, right: int) -> int:
+        return self.sums[right] - (self.sums[left-1] if left else 0)
+
+
+
+def test(data_test):
+    s = Solution()
+    data = data_test  # normal
+    # data = [list2node(data_test[0])]  # list转node
+    return s.getResult(*data)
+
+
+def test_obj(data_test):
+    result = [None]
+    obj = NumArray(*data_test[1][0])
+    for fun, data in zip(data_test[0][1::], data_test[1][1::]):
+        if data:
+            res = obj.__getattribute__(fun)(*data)
+        else:
+            res = obj.__getattribute__(fun)()
+        result.append(res)
+    return result
+
+
+if __name__ == '__main__':
+    datas = [
+        [["NumArray","sumRange","sumRange","sumRange"],[[[-2,0,3,-5,2,-1]],[0,2],[2,5],[0,5]]],
+    ]
+    for data_test in datas:
+        t0 = time.time()
+        print('-' * 50)
+        print('input:', data_test)
+        print('output:', test_obj(data_test))
+        print(f'use time:{time.time() - t0}s')
