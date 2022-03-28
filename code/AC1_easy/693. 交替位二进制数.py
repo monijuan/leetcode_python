649e9408a7960dc2a6ca9b672c313f3bebf2fe99
monijuan <1019646292@qq.com>
2022-03-28 21:21:32 +0800

    693

---
+++ code/AC1_easy/693. 交替位二进制数.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/3/28 8:49
+# @Author  : 模拟卷
+# @Github  : https://github.com/monijuan
+# @CSDN    : https://blog.csdn.net/qq_34451909
+# @File    : 693. 交替位二进制数.py
+# @Software: PyCharm 
+# ===================================
+"""给定一个正整数，检查它的二进制表示是否总是 0、1 交替出现：换句话说，就是二进制表示中相邻两位的数字永不相同。
+
+ 
+
+示例 1：
+
+输入：n = 5
+输出：true
+解释：5 的二进制表示是：101
+示例 2：
+
+输入：n = 7
+输出：false
+解释：7 的二进制表示是：111.
+示例 3：
+
+输入：n = 11
+输出：false
+解释：11 的二进制表示是：1011.
+ 
+
+提示：
+
+1 <= n <= 231 - 1
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-number-with-alternating-bits
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+from leetcode_python.utils import *
+
+class Solution:
+    def hasAlternatingBits(self, n: int) -> bool:
+        # print(n,bin(n),bin(n^(n>>1)))
+        return '0' not in bin(n^(n>>1))[2:]
+
+
+def test(data_test):
+    s = Solution()
+    data = data_test    # normal
+    # data = [List2Node(data_test[0])]  # list转node
+    return s.hasAlternatingBits(*data)
+
+def test_obj(data_test):
+    result = [None]
+    obj = Solution(*data_test[1][0])
+    for fun,data in zip(data_test[0][1::],data_test[1][1::]):
+        if data:
+            res = obj.__getattribute__(fun)(*data)
+        else:
+            res = obj.__getattribute__(fun)()
+        result.append(res)
+    return result
+
+if __name__ == '__main__':
+    datas = [
+        [10],
+    ]
+    for data_test in datas:
+        t0 = time.time()
+        print('-'*50)
+        print('input:', data_test)
+        print('output:', test(data_test))
+        print(f'use time:{time.time() - t0}s')
