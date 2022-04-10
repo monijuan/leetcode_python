62be6623c63a8a7930dd179d7669f54818b59e3a
monijuan <1019646292@qq.com>
2022-04-10 12:30:28 +0800

    804

---
+++ code/AC1_easy/804. 唯一摩尔斯密码词.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/4/10 12:27
+# @Author  : 模拟卷
+# @Github  : https://github.com/monijuan
+# @CSDN    : https://blog.csdn.net/qq_34451909
+# @File    : 804. 唯一摩尔斯密码词.py
+# @Software: PyCharm 
+# ===================================
+"""国际摩尔斯密码定义一种标准编码方式，将每个字母对应于一个由一系列点和短线组成的字符串， 比如:
+
+'a' 对应 ".-" ，
+'b' 对应 "-..." ，
+'c' 对应 "-.-." ，以此类推。
+为了方便，所有 26 个英文字母的摩尔斯密码表如下：
+
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+给你一个字符串数组 words ，每个单词可以写成每个字母对应摩尔斯密码的组合。
+
+例如，"cab" 可以写成 "-.-..--..." ，(即 "-.-." + ".-" + "-..." 字符串的结合)。我们将这样一个连接过程称作 单词翻译 。
+对 words 中所有单词进行单词翻译，返回不同 单词翻译 的数量。
+
+ 
+
+示例 1：
+
+输入: words = ["gin", "zen", "gig", "msg"]
+输出: 2
+解释:
+各单词翻译如下:
+"gin" -> "--...-."
+"zen" -> "--...-."
+"gig" -> "--...--."
+"msg" -> "--...--."
+
+共有 2 种不同翻译, "--...-." 和 "--...--.".
+示例 2：
+
+输入：words = ["a"]
+输出：1
+ 
+
+提示：
+
+1 <= words.length <= 100
+1 <= words[i].length <= 12
+words[i] 由小写英文字母组成
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/unique-morse-code-words
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+from leetcode_python.utils import *
+
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        sss = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        change = lambda word:''.join(sss[ord(c)-ord('a')] for c in word)
+        return len(set(change(w) for w in words))
+
+
+
+def test(data_test):
+    s = Solution()
+    data = data_test    # normal
+    # data = [List2Node(data_test[0])]  # list转node
+    return s.getResult(*data)
+
+def test_obj(data_test):
+    result = [None]
+    obj = Solution(*data_test[1][0])
+    for fun,data in zip(data_test[0][1::],data_test[1][1::]):
+        if data:
+            res = obj.__getattribute__(fun)(*data)
+        else:
+            res = obj.__getattribute__(fun)()
+        result.append(res)
+    return result
+
+if __name__ == '__main__':
+    datas = [
+        [],
+    ]
+    for data_test in datas:
+        t0 = time.time()
+        print('-'*50)
+        print('input:', data_test)
+        print('output:', test(data_test))
+        print(f'use time:{time.time() - t0}s')
