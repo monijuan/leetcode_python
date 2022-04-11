c301d02c810bdab6ddde6b022f6560d9c8440113
monijuan <1019646292@qq.com>
2022-04-11 11:11:38 +0800

    1855 357 633

---
+++ code/AC2_normal/1855. 下标对中的最大距离.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/4/11 10:06
+# @Author  : 模拟卷
+# @Github  : https://github.com/monijuan
+# @CSDN    : https://blog.csdn.net/qq_34451909
+# @File    : 1855. 下标对中的最大距离.py
+# @Software: PyCharm 
+# ===================================
+"""给你两个 非递增 的整数数组 nums1​​​​​​ 和 nums2​​​​​​ ，数组下标均 从 0 开始 计数。
+
+下标对 (i, j) 中 0 <= i < nums1.length 且 0 <= j < nums2.length 。如果该下标对同时满足 i <= j 且 nums1[i] <= nums2[j] ，则称之为 有效 下标对，该下标对的 距离 为 j - i​​ 。​​
+
+返回所有 有效 下标对 (i, j) 中的 最大距离 。如果不存在有效下标对，返回 0 。
+
+一个数组 arr ，如果每个 1 <= i < arr.length 均有 arr[i-1] >= arr[i] 成立，那么该数组是一个 非递增 数组。
+
+ 
+
+示例 1：
+
+输入：nums1 = [55,30,5,4,2], nums2 = [100,20,10,10,5]
+输出：2
+解释：有效下标对是 (0,0), (2,2), (2,3), (2,4), (3,3), (3,4) 和 (4,4) 。
+最大距离是 2 ，对应下标对 (2,4) 。
+示例 2：
+
+输入：nums1 = [2,2,2], nums2 = [10,10,1]
+输出：1
+解释：有效下标对是 (0,0), (0,1) 和 (1,1) 。
+最大距离是 1 ，对应下标对 (0,1) 。
+示例 3：
+
+输入：nums1 = [30,29,19,5], nums2 = [25,25,25,25,25]
+输出：2
+解释：有效下标对是 (2,2), (2,3), (2,4), (3,3) 和 (3,4) 。
+最大距离是 2 ，对应下标对 (2,4) 。
+ 
+
+提示：
+
+1 <= nums1.length <= 105
+1 <= nums2.length <= 105
+1 <= nums1[i], nums2[j] <= 105
+nums1 和 nums2 都是 非递增 数组
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/maximum-distance-between-a-pair-of-values
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+from leetcode_python.utils import *
+
+class Solution:
+    def maxDistance(self, nums1: List[int], nums2: List[int]) -> int:
+        n1, n2 = len(nums1), len(nums2)
+        i = 0
+        res = 0
+        for j in range(n2):
+            while i < n1 and nums1[i] > nums2[j]:
+                i += 1
+            if i < n1:
+                res = max(res, j - i)
+        return res
+
+
+def test(data_test):
+    s = Solution()
+    data = data_test    # normal
+    # data = [List2Node(data_test[0])]  # list转node
+    return s.getResult(*data)
+
+def test_obj(data_test):
+    result = [None]
+    obj = Solution(*data_test[1][0])
+    for fun,data in zip(data_test[0][1::],data_test[1][1::]):
+        if data:
+            res = obj.__getattribute__(fun)(*data)
+        else:
+            res = obj.__getattribute__(fun)()
+        result.append(res)
+    return result
+
+if __name__ == '__main__':
+    datas = [
+        [],
+    ]
+    for data_test in datas:
+        t0 = time.time()
+        print('-'*50)
+        print('input:', data_test)
+        print('output:', test(data_test))
+        print(f'use time:{time.time() - t0}s')
---
+++ code/AC2_normal/357. 统计各位数字都不同的数字个数.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/4/11 8:57
+# @Author  : 模拟卷
+# @Github  : https://github.com/monijuan
+# @CSDN    : https://blog.csdn.net/qq_34451909
+# @File    : 357. 统计各位数字都不同的数字个数.py
+# @Software: PyCharm 
+# ===================================
+"""给你一个整数 n ，统计并返回各位数字都不同的数字 x 的个数，其中 0 <= x < 10n 。
+ 
+
+示例 1：
+
+输入：n = 2
+输出：91
+解释：答案应为除去 11、22、33、44、55、66、77、88、99 外，在 0 ≤ x < 100 范围内的所有数字。
+示例 2：
+
+输入：n = 0
+输出：1
+ 
+
+提示：
+
+0 <= n <= 8
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/count-numbers-with-unique-digits
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+from leetcode_python.utils import *
+
+class Solution:
+    def countNumbersWithUniqueDigits(self, n: int) -> int:
+        if n == 0:
+            return 1
+        if n == 1:
+            return 10
+        res, cur = 10, 9
+        for i in range(n - 1):
+            cur *= 9 - i
+            res += cur
+        return res
+
+def test(data_test):
+    s = Solution()
+    data = data_test    # normal
+    # data = [List2Node(data_test[0])]  # list转node
+    return s.getResult(*data)
+
+def test_obj(data_test):
+    result = [None]
+    obj = Solution(*data_test[1][0])
+    for fun,data in zip(data_test[0][1::],data_test[1][1::]):
+        if data:
+            res = obj.__getattribute__(fun)(*data)
+        else:
+            res = obj.__getattribute__(fun)()
+        result.append(res)
+    return result
+
+if __name__ == '__main__':
+    datas = [
+        [],
+    ]
+    for data_test in datas:
+        t0 = time.time()
+        print('-'*50)
+        print('input:', data_test)
+        print('output:', test(data_test))
+        print(f'use time:{time.time() - t0}s')
