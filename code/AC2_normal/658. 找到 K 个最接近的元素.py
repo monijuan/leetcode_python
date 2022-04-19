31d4f0bccdfde7c248d5252390e562c555bcfc17
monijuan <1019646292@qq.com>
2022-04-19 09:21:49 +0800

    821 658

---
+++ code/AC2_normal/658. 找到 K 个最接近的元素.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/4/19 9:16
+# @Author  : 模拟卷
+# @Github  : https://github.com/monijuan
+# @CSDN    : https://blog.csdn.net/qq_34451909
+# @File    : 658. 找到 K 个最接近的元素.py
+# @Software: PyCharm 
+# ===================================
+"""给定一个 排序好 的数组 arr ，两个整数 k 和 x ，从数组中找到最靠近 x（两数之差最小）的 k 个数。返回的结果必须要是按升序排好的。
+
+整数 a 比整数 b 更接近 x 需要满足：
+
+|a - x| < |b - x| 或者
+|a - x| == |b - x| 且 a < b
+ 
+
+示例 1：
+
+输入：arr = [1,2,3,4,5], k = 4, x = 3
+输出：[1,2,3,4]
+示例 2：
+
+输入：arr = [1,2,3,4,5], k = 4, x = -1
+输出：[1,2,3,4]
+ 
+
+提示：
+
+1 <= k <= arr.length
+1 <= arr.length <= 104
+arr 按 升序 排列
+-104 <= arr[i], x <= 104
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/find-k-closest-elements
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+from leetcode_python.utils import *
+
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        return sorted(sorted(arr,key=lambda a:(abs(a-x),a))[:k])
+
+
+
+def test(data_test):
+    s = Solution()
+    data = data_test    # normal
+    # data = [List2Node(data_test[0])]  # list转node
+    return s.getResult(*data)
+
+def test_obj(data_test):
+    result = [None]
+    obj = Solution(*data_test[1][0])
+    for fun,data in zip(data_test[0][1::],data_test[1][1::]):
+        if data:
+            res = obj.__getattribute__(fun)(*data)
+        else:
+            res = obj.__getattribute__(fun)()
+        result.append(res)
+    return result
+
+if __name__ == '__main__':
+    datas = [
+        [],
+    ]
+    for data_test in datas:
+        t0 = time.time()
+        print('-'*50)
+        print('input:', data_test)
+        print('output:', test(data_test))
+        print(f'use time:{time.time() - t0}s')
