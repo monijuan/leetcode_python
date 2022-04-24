0eb4324c4d2358a66cdf05f3f532ce4f3e626e28
monijuan <1019646292@qq.com>
2022-04-24 12:16:11 +0800

    868

---
+++ code/AC1_easy/868. 二进制间距.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/4/24 12:10
+# @Author  : 模拟卷
+# @Github  : https://github.com/monijuan
+# @CSDN    : https://blog.csdn.net/qq_34451909
+# @File    : 868. 二进制间距.py
+# @Software: PyCharm 
+# ===================================
+"""给定一个正整数 n，找到并返回 n 的二进制表示中两个 相邻 1 之间的 最长距离 。如果不存在两个相邻的 1，返回 0 。
+
+如果只有 0 将两个 1 分隔开（可能不存在 0 ），则认为这两个 1 彼此 相邻 。两个 1 之间的距离是它们的二进制表示中位置的绝对差。例如，"1001" 中的两个 1 的距离为 3 。
+
+ 
+
+示例 1：
+
+输入：n = 22
+输出：2
+解释：22 的二进制是 "10110" 。
+在 22 的二进制表示中，有三个 1，组成两对相邻的 1 。
+第一对相邻的 1 中，两个 1 之间的距离为 2 。
+第二对相邻的 1 中，两个 1 之间的距离为 1 。
+答案取两个距离之中最大的，也就是 2 。
+示例 2：
+
+输入：n = 8
+输出：0
+解释：8 的二进制是 "1000" 。
+在 8 的二进制表示中没有相邻的两个 1，所以返回 0 。
+示例 3：
+
+输入：n = 5
+输出：2
+解释：5 的二进制是 "101" 。
+ 
+
+提示：
+
+1 <= n <= 109
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-gap
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+from leetcode_python.utils import *
+
+class Solution:
+    def binaryGap(self, n: int) -> int:
+        s = bin(n)
+        l = s.find('1')
+        length = len(s)
+        res = 0
+        # print(s)
+        while l<length:
+            r = l+1
+            while r<length and s[r]!='1':
+                r+=1
+            if r<length:
+                res = max(res,r-l)
+            l=r
+        return res
+
+
+def test(data_test):
+    s = Solution()
+    data = data_test    # normal
+    # data = [List2Node(data_test[0])]  # list转node
+    return s.binaryGap(*data)
+
+def test_obj(data_test):
+    result = [None]
+    obj = Solution(*data_test[1][0])
+    for fun,data in zip(data_test[0][1::],data_test[1][1::]):
+        if data:
+            res = obj.__getattribute__(fun)(*data)
+        else:
+            res = obj.__getattribute__(fun)()
+        result.append(res)
+    return result
+
+if __name__ == '__main__':
+    datas = [
+        [8],
+    ]
+    for data_test in datas:
+        t0 = time.time()
+        print('-'*50)
+        print('input:', data_test)
+        print('output:', test(data_test))
+        print(f'use time:{time.time() - t0}s')
