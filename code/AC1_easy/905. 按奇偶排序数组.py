12dba014e44c98442cd80e59ac5fa87d5252f59f
monijuan <1019646292@qq.com>
2022-04-28 16:22:35 +0800

    905

---
+++ code/AC1_easy/905. 按奇偶排序数组.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/4/28 9:59
+# @Author  : 模拟卷
+# @Github  : https://github.com/monijuan
+# @CSDN    : https://blog.csdn.net/qq_34451909
+# @File    : 905. 按奇偶排序数组.py
+# @Software: PyCharm 
+# ===================================
+"""给你一个整数数组 nums，将 nums 中的的所有偶数元素移动到数组的前面，后跟所有奇数元素。
+
+返回满足此条件的 任一数组 作为答案。
+
+ 
+
+示例 1：
+
+输入：nums = [3,1,2,4]
+输出：[2,4,3,1]
+解释：[4,2,3,1]、[2,4,1,3] 和 [4,2,1,3] 也会被视作正确答案。
+示例 2：
+
+输入：nums = [0]
+输出：[0]
+ 
+
+提示：
+
+1 <= nums.length <= 5000
+0 <= nums[i] <= 5000
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/sort-array-by-parity
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+from leetcode_python.utils import *
+
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        n1,n2=[],[]
+        for n in nums:
+            if n&1:n1.append(n)
+            else:n2.append(n)
+        return n2+n1
+
+
+def test(data_test):
+    s = Solution()
+    data = data_test    # normal
+    # data = [List2Node(data_test[0])]  # list转node
+    return s.getResult(*data)
+
+def test_obj(data_test):
+    result = [None]
+    obj = Solution(*data_test[1][0])
+    for fun,data in zip(data_test[0][1::],data_test[1][1::]):
+        if data:
+            res = obj.__getattribute__(fun)(*data)
+        else:
+            res = obj.__getattribute__(fun)()
+        result.append(res)
+    return result
+
+if __name__ == '__main__':
+    datas = [
+        [],
+    ]
+    for data_test in datas:
+        t0 = time.time()
+        print('-'*50)
+        print('input:', data_test)
+        print('output:', test(data_test))
+        print(f'use time:{time.time() - t0}s')
